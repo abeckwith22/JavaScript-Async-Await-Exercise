f0acd8ea3a145d6edd1146620e7eb8f4076ce8f6
abeckwith22 <140443409+abeckwith22@users.noreply.github.com>
2024-04-08 02:57:58 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,91 @@
+/* // -- RANDOM FACTS API --
+let card_button = document.querySelector('.card-button');
+const card_area = document.querySelector('.cards-area');
+
+function create_append_element(text){
+    const new_div = document.createElement('div');
+    const new_p = document.createElement('p');
+
+    // set classes
+    new_div.classList.toggle('card')
+
+    new_p.innerText = text;
+    new_div.append(new_p);
+    card_area.append(new_div);
+}
+
+card_button.addEventListener("click", async (event) => {
+    event.preventDefault();
+    card_area.innerHTML = "";
+    let text_field = document.querySelector('.card-text');
+    let text_value = text_field.value;
+    for (let i = 0; i < 4; i++){
+        let URL = `http://numbersapi.com/${text_value}?json`;
+        let response = await axios.get(URL);
+        let text = response.data.text;
+        create_append_element(text);
+    }
+    return response;
+});
+
+*/
+
+// -- Deck of Cards Exercise -- Part 2
+
+const card_form = document.querySelector('.card-form');
+const card_area = document.querySelector('.card-area');
+
+let get_deck_url = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
+
+const new_deck_button = document.querySelector('.new-deck');
+new_deck_button.addEventListener("click", async (event) => {
+    var card_id = 0;
+    event.preventDefault();
+    const card_area = document.querySelector('.card-area');
+    card_area.innerHTML = "";
+    const response = await axios.get(get_deck_url);
+    const data = response.data;
+
+    sessionStorage.setItem('deck_id', data.deck_id); // set deck_id to be remembered
+    play('card_shuffle.wav');
+
+});
+
+const new_card_button = document.querySelector('.new-card');
+new_card_button.addEventListener("click", async (event) => {
+    event.preventDefault();
+    let get_card_from_deck_url = `https://deckofcardsapi.com/api/deck/${sessionStorage.getItem('deck_id')}/draw/`
+    const response = await axios.get(get_card_from_deck_url);
+    const data = response.data;
+    console.log(data);
+    const cards = [...data.cards];
+    for(let i = 0; i < cards.length; i++){
+        create_card(cards[i]);
+    }
+    play('draw_card.mp3')
+});
+
+function create_card(obj){
+    const card_area = document.querySelector('.card-area');
+    const new_card = document.createElement('div');
+    const img = document.createElement('img');
+    new_card.classList.toggle('card');
+    new_card.id = 
+
+    img.src = obj.image;
+    new_card.append(img);
+    rotateCard(new_card);
+    card_area.append(new_card);
+}
+
+let rotation =  0;
+
+function rotateCard(card) {
+    let rotation = Math.floor(Math.random() * 360);
+    card.style.transform = `rotate(${rotation}deg)`;
+}
+
+function play(audio) {
+    let sound = new Audio(audio);
+    sound.play();
+}
